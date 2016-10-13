eebf85f5500e58dc25cb3bcb5149f915278bd9a1
Olivia <oliviantaylor@gmail.com>
2016-10-13 17:53:27 -0400

    podfiles and image reference added - afnetworking cocoapod installed

--- CloudCamera/ViewController.m
+++ CloudCamera/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+@import Firebase;
+@import FirebaseStorage;
 
 @interface ViewController ()
 
@@ -16,7 +18,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //reference to downloaded image in firebase
+    
+    FIRStorage *storage = [FIRStorage storage];
+    FIRStorageReference *storageRef = [storage referenceForURL:@"gs://cloudcamera-497b1.appspot.com>"];
+    FIRStorageReference *imagesRef = [storageRef child:@"images"];
+    
+    NSString *fileName = @"teddy_icon.jpeg";
+    FIRStorageReference *teddyRef = [imagesRef child:fileName];
+    
+    NSString *path = teddyRef.fullPath;
+    NSString *name = teddyRef.name;
+    
 }
 
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+target 'CloudCamera' do
+  use_frameworks!
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  
+  source 'https://github.com/CocoaPods/Specs.git'
+  platform :ios, '7.0'
+  pod 'AFNetworking', '~> 2.5'
+end
