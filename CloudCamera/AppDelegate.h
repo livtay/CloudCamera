94d49bcea6b854356c7349147c9e1e3c30ee0bd3
Olivia <oliviantaylor@gmail.com>
2016-10-13 16:48:39 -0400

    Initial Commit

---
+++ CloudCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudCamera
+//
+//  Created by Olivia Taylor on 10/13/16.
+//  Copyright © 2016 Olivia Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
